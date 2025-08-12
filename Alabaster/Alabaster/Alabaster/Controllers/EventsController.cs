0205b26f6a1362131f849a4cc27b3f4703836ac6
kamil maharaj <ST10068456@vcconnect.edu.za>
2025-08-12 18:45:54 +0200

    Alabaster website
    added past events page and did styling

--- Alabaster/Alabaster/Alabaster/Controllers/EventsController.cs
+++ Alabaster/Alabaster/Alabaster/Controllers/EventsController.cs
@@ -5,6 +5,7 @@ using Firebase.Database.Query;
 using System.Linq;
 using System.Threading.Tasks;
 using System;
+using System.Collections.Generic;
 
 namespace Alabaster.Controllers
 {
@@ -20,9 +21,25 @@ namespace Alabaster.Controllers
         // GET: /Events
         public async Task<IActionResult> Index()
         {
+            // Load all events from Firebase
             var events = await _firebase.Child("Events").OnceAsync<UpcomingEvent>();
-            var eventList = events.Select(e => e.Object).ToList();
-            return View(eventList);
+            var eventList = events.Select(e =>
+            {
+                var ev = e.Object;
+                ev.Id = e.Key; // store Firebase ID
+                return ev;
+            }).ToList();
+
+            // Move past events to PastEvents
+            await MovePastEvents(eventList);
+
+            // Filter only upcoming events
+            var upcomingEvents = eventList
+                .Where(e => DateTime.TryParse(e.Date, out DateTime date) && date >= DateTime.Today)
+                .OrderBy(e => DateTime.Parse(e.Date))
+                .ToList();
+
+            return View(upcomingEvents);
         }
 
         // GET: /Events/AddEvent
@@ -49,7 +66,12 @@ namespace Alabaster.Controllers
         public async Task<IActionResult> Volunteer(string eventId)
         {
             var events = await _firebase.Child("Events").OnceAsync<UpcomingEvent>();
-            var eventList = events.Select(e => e.Object).ToList();
+            var eventList = events.Select(e =>
+            {
+                var ev = e.Object;
+                ev.Id = e.Key;
+                return ev;
+            }).ToList();
             ViewBag.Events = eventList;
 
             Volunteer volunteer = new Volunteer();
@@ -76,7 +98,13 @@ namespace Alabaster.Controllers
                 if (!string.IsNullOrEmpty(model.EventId))
                 {
                     var events = await _firebase.Child("Events").OnceAsync<UpcomingEvent>();
-                    var selectedEvent = events.Select(e => e.Object).FirstOrDefault(e => e.Id == model.EventId);
+                    var selectedEvent = events.Select(e =>
+                    {
+                        var ev = e.Object;
+                        ev.Id = e.Key;
+                        return ev;
+                    }).FirstOrDefault(e => e.Id == model.EventId);
+
                     if (selectedEvent != null)
                     {
                         model.EventName = selectedEvent.Name;
@@ -95,7 +123,6 @@ namespace Alabaster.Controllers
                 }
             }
 
-            // Reload events for dropdown if ModelState invalid or save failed
             var allEvents = await _firebase.Child("Events").OnceAsync<UpcomingEvent>();
             ViewBag.Events = allEvents.Select(e => e.Object).ToList();
 
@@ -107,6 +134,44 @@ namespace Alabaster.Controllers
         public IActionResult VolunteerThankYou()
         {
             return View();
+        }
+
+        // GET: /Events/Past
+        [HttpGet]
+        public async Task<IActionResult> Past()
+        {
+            var pastEvents = await _firebase.Child("PastEvents").OnceAsync<UpcomingEvent>();
+            var pastList = pastEvents.Select(e =>
+            {
+                var ev = e.Object;
+                ev.Id = e.Key;
+                return ev;
+            })
+            .OrderByDescending(e => DateTime.Parse(e.Date))
+            .ToList();
+
+            return View(pastList);
+        }
+
+        // Move past events from Events to PastEvents in Firebase
+        private async Task MovePastEvents(List<UpcomingEvent> eventList)
+        {
+            DateTime today = DateTime.Today;
+
+            foreach (var evt in eventList.ToList()) // work on a copy
+            {
+                if (DateTime.TryParse(evt.Date, out DateTime eventDate) && eventDate < today)
+                {
+                    // Add to PastEvents
+                    await _firebase.Child("PastEvents").PostAsync(evt);
+
+                    // Remove from Events
+                    await _firebase.Child("Events").Child(evt.Id).DeleteAsync();
+
+                    // Remove locally so it won't be shown as upcoming
+                    eventList.Remove(evt);
+                }
+            }
         }
     }
 }
